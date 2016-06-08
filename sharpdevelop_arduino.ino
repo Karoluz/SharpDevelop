8c708b68aad0ece5b788f65c0b8f490cb9bd097f
Karoluz <karoluzibarra@gmail.com>
2016-06-08 03:21:12 +0000

    Add files via upload
    This is the code build to Arduino Uno.
    To receive the signal from Serial port and activate the color selected in the Application

---
+++ sharpdevelop_arduino.ino
@@ -0,0 +1,49 @@
+//Arduio Control
+int ledPinRED   = 13;
+int ledPinBLUE  = 12;
+int ledPinGREEN = 11;
+int incomingByte = 0;
+//the setup() method runs once, when the sketch starts
+void setup() 
+{
+  //initialize the digital pin as an output
+  pinMode(ledPinRED,   OUTPUT);
+  pinMode(ledPinBLUE,  OUTPUT);
+  pinMode(ledPinGREEN, OUTPUT);
+  Serial.begin(9600);
+}
+//the loop() method runs over and over again,
+//as long as the Arduio has power
+void loop() 
+{
+  //read the incoming byte
+  incomingByte = Serial.read();
+  if (incomingByte != -1);
+  {
+    Serial.println(incomingByte);
+    if (incomingByte == 112)//p
+    {
+      digitalWrite(ledPinRED, HIGH);
+      digitalWrite(ledPinBLUE, LOW);
+      digitalWrite(ledPinGREEN, LOW);
+    }
+    else if (incomingByte ==97)//a
+    {
+      digitalWrite(ledPinBLUE, HIGH);
+      digitalWrite(ledPinRED, LOW);
+      digitalWrite(ledPinGREEN, LOW);
+    }
+    else if (incomingByte == 114)//r
+    {
+      digitalWrite(ledPinGREEN, HIGH);
+      digitalWrite(ledPinRED, LOW);
+      digitalWrite(ledPinBLUE, LOW);
+    }
+    else if (incomingByte == 115)//s
+    {
+      digitalWrite(ledPinRED, LOW);
+      digitalWrite(ledPinBLUE, LOW);
+      digitalWrite(ledPinGREEN, LOW);
+    }
+  }
+}
